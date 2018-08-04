515877a3984545baad2c4d9aad31dd8ea8021ba7
mschuldt <mbschuldt@gmail.com>
2018-08-04 01:33:19 -0700

    exit if not root

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "must be run as root"
+    exit 1
+fi
+
 echo -n "gforth-fast  " >> log
 echo -n $(git rev-parse HEAD) " " >> log
 x=`nice -n -20 gforth-fast run.fth`
