585012ac6daf851eb1686f2060af92bc7cb4745b
mschuldt <mbschuldt@gmail.com>
2018-08-24 11:35:48 -0700

    benchmark version number

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-echo -n "gforth-fast  " >> log
+echo -n "1 gforth-fast  " >> log
 echo -n $(git rev-parse HEAD) " " >> log
 x=`nice -n -20 gforth-fast run.fth`
 echo $x
