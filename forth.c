c3895598c3f532a287af00bc905a452c85986166
mschuldt <mbschuldt@gmail.com>
2019-06-20 16:29:50 -0500

    formatting

--- forth.c
+++ forth.c
@@ -71,9 +71,11 @@ int parse_num(char *a, cell len, cell* res) {
   int b = base;
   int isminus = 0;
   cell n=0;
-  if (len <= 0) return false;
+  if (len <= 0) {
+    return false;
+  }
   switch (*a) {
-  case '%': b = 2; len--; a++; break;
+  case '%': b = 2;  len--; a++; break;
   case '#': b = 10; len--; a++; break;
   case '$': b = 16; len--; a++; break;
   }
@@ -84,7 +86,9 @@ int parse_num(char *a, cell len, cell* res) {
   }
   for( char c; len > 0; len-- ) {
     c = *a++;
-    if (c < '0' || c > '9') return false;
+    if (c < '0' || c > '9') {
+      return false;
+    }
     n = (n * b) + c - '0';
   }
   *res = isminus ? -n : n;
@@ -95,7 +99,9 @@ static char *_line = (char *)NULL;
 int read_stdin(){ // read a line from stdin into tib
   printf(" ok\r\n");
   char* in = readline("");
-  if (!in) exit(0);
+  if (!in) {
+    exit(0);
+  }
   strcpy(tib, in);
   free(in);
   return 1;
@@ -105,13 +111,17 @@ int read_line() {
   lineno++;
   colno = 0;
   tib_i = 0;
-  if (input_device == stdin ) return read_stdin();
+  if (input_device == stdin ) {
+    return read_stdin();
+  }
   return fgets(tib, 128, input_device) != NULL;
 }
 
 char next_char(){
   colno++;
-  if (tib_i  >= tib_max) return 0;
+  if (tib_i  >= tib_max) {
+    return 0;
+  }
   return tib[tib_i++];
 }
 
@@ -121,18 +131,24 @@ int word_c = 0;
 int parse_name(){ // return true on success
   char c;
   word_c = 0;
-  do{ c = next_char(); } while (c && isspace(c));
-  if (c == 0) return 0;
+  do{
+    c = next_char();
+  } while (c && isspace(c));
+  if (c == 0) {
+    return 0;
+  }
   word_a = tib + tib_i - 1;
   word_c = 1;
-  while(!isspace(c = next_char()) && c) ++word_c;
+  while(!isspace(c = next_char()) && c) {
+    ++word_c;
+  }
   tib_i--;
   return 1;
 }
 
 void dict_append_str(char *a, int c){
-    strncpy((char*)(dp), a, c);
-    dp += c / bytes_per_cell + c % bytes_per_cell;
+  strncpy((char*)(dp), a, c);
+  dp += c / bytes_per_cell + c % bytes_per_cell;
 }
 
 void create(char* a, cell c){
@@ -149,14 +165,22 @@ void code(char* name, void* addr){
 }
 
 int str_eq(char *a1, u_cell c1, char *a2, u_cell c2){
-  if ( c1 != c2 ) return false;
-  for (; c1 && c2; a1++, a2++, c1--, c2--) if (*a1 != *a2) return false;
+  if ( c1 != c2 ) {
+    return false;
+  }
+  for (; c1 && c2; a1++, a2++, c1--, c2--) {
+    if (*a1 != *a2) {
+      return false;
+    }
+  }
   return true;
 }
 
 int compare(char *a1, u_cell c1, char *a2, u_cell c2) {
   for (;c1 && c2; a1++, a2++, c1--, c2--) {
-    if (*a1 != *a2) return (*a1 < *a2) ? -1 : 1;
+    if (*a1 != *a2) {
+      return (*a1 < *a2) ? -1 : 1;
+    }
     a1++; a2++; c1--; c2--;
   }
   return c1 == c2 ? 0 : ((c1 < c2) ? -1 : 1);
@@ -185,7 +209,9 @@ cell* find_word(char* a, int c, cell* imm){
 void cr(){ printf("\n"); };
 
 void type(char* a, int c){
-  for ( ; c>0; c--, a++) printf("%c", *a);
+  for ( ; c>0; c--, a++) {
+    printf("%c", *a);
+  }
 }
 
 void words(){
@@ -237,7 +263,9 @@ cell* do_interpret(){
     return handle_word(word);
   }
   int ok = parse_num(word_a, word_c, &num);
-  if (ok) return handle_num(num);
+  if (ok) {
+    return handle_num(num);
+  }
   print_word(word_a, word_c);
   exit(1);
   return LIT_XT;
@@ -252,22 +280,30 @@ cell* get_xt(char* word){
   return cfa(x);
 }
 
-int depth(){ return (sp - sp0) / 4 + 1; }
+int depth(){
+  return (sp - sp0) / 4 + 1;
+}
 
 void print_stack(){
   printf("<%d> ", depth());   //TODO: depth is wrong
-  for (cell* p = sp0+1; p < sp; p++) printf("%lld ", (cell)*p);
+  for (cell* p = sp0+1; p < sp; p++) {
+    printf("%lld ", (cell)*p);
+  }
   printf("%lld\n", tos);
 }
 
 void cmove(char *from, char *to, u_cell length) {
-    while ((length--) != 0) *to++ = *from++;
+  while ((length--) != 0) {
+    *to++ = *from++;
+  }
 }
 
 void see(char* a, int c){
   cell imm;
   cell* word = find_word(word_a, word_c, &imm);
-  if (! word) error("undefined");
+  if (! word) {
+    error("undefined");
+  }
   int len = *word & LEN_MASK;
   char* name = (char*)(word+1);
   if (*word & CODE_BIT) {
@@ -279,7 +315,9 @@ void see(char* a, int c){
   for(; (cell*)(*code) != EXIT_XT; code++) {
     printf("   %llu:   %llu\n", (cell)code, *code);
   }
-  if (imm) printf("  ; immediate\n");
+  if (imm) {
+    printf("  ; immediate\n");
+  }
   else printf("  ;\n");
 }
 
@@ -312,36 +350,73 @@ void forth(){
   rp = rp0;
   while (true) {
     if (!read_line()){
-      if(input_device != stdin) fclose(input_device);
+      if(input_device != stdin) {
+        fclose(input_device);
+      }
       input_device = stdin;
       continue; //TODO: fix potential loop
     }
   CODE("interpret", interpret):
     while (parse_name()){
       xt = do_interpret();
-      if (xt) goto execute_xt;
+      if (xt) {
+        goto execute_xt;
+      }
     }
   }
  docol:
   rpush(ip);
   ip = (cell*)(*(ip-1)) + 1;
   NEXT;
- CODE("exit", exit): ip = (cell*)(*--rp); NEXT;
- CODE("drop", drop): tos = *--sp; NEXT;
- CODE("2drop", _2drop): sp--; tos = *--sp; NEXT;
- CODE("dup", dup): *sp++ = tos; NEXT;
- CODE("2dup", _2dup): *sp++ = tos; *sp++ = *(sp-3); NEXT;
- CODE("over", over): push(*(sp-2)); NEXT;
- CODE("nip", nip): --sp; NEXT;
- CODE("swap", swap): x = *(sp-1); *(sp-1) = tos; tos = x; NEXT;
- CODE("pick", pick): tos = *(sp-1-tos); NEXT;
- CODE("rpick", rpick): tos = *(rp-1-tos); NEXT;
+ CODE("exit", exit):
+  ip = (cell*)(*--rp);
+  NEXT;
+ CODE("drop", drop):
+  tos = *--sp;
+  NEXT;
+ CODE("2drop", _2drop):
+  sp--;
+  tos = *--sp;
+  NEXT;
+ CODE("dup", dup):
+  *sp++ = tos;
+  NEXT;
+ CODE("2dup", _2dup):
+  *sp++ = tos;
+  *sp++ = *(sp-3);
+  NEXT;
+ CODE("over", over):
+  push(*(sp-2));
+  NEXT;
+ CODE("nip", nip):
+  --sp;
+  NEXT;
+ CODE("swap", swap):
+  x = *(sp-1);
+  *(sp-1) = tos;
+  tos = x;
+  NEXT;
+ CODE("pick", pick):
+  tos = *(sp-1-tos);
+  NEXT;
+ CODE("rpick", rpick):
+  tos = *(rp-1-tos);
+  NEXT;
  CODE("rot", rot):
-  x = *(sp-2); *(sp-2) = *(sp-1); *(sp-1) = tos; tos = x; NEXT;
+  x = *(sp-2);
+  *(sp-2) = *(sp-1);
+  *(sp-1) = tos;
+  tos = x;
+  NEXT;
  CODE("-rot", _rot):
-  x = *(sp-2); *(sp-2) = tos; tos=*(sp-1); *(sp-1) = x; NEXT;
-
-#define BINOP(name, label, op) CODE(name, label): tos = *--sp op tos; NEXT;
+  x = *(sp-2);
+  *(sp-2) = tos;
+  tos=*(sp-1);
+  *(sp-1) = x;
+  NEXT;
+#define BINOP(name, label, op) CODE(name, label):       \
+  tos = *--sp op tos;                                   \
+  NEXT;
   BINOP("+", plus, +);
   BINOP("-", minus, -);
   BINOP("*", mul, *);
@@ -355,60 +430,148 @@ void forth(){
   BINOP(">", greater_than, >);
   BINOP(">=", greater_or_eq, >=);
   BINOP("<=", less_or_eq, <=);
- CODE("1+", one_plus): tos += 1; NEXT;
- CODE("1-", one_minus): tos -= 1; NEXT;
- CODE("0=", zero_eq): tos = (tos == 0); NEXT;
- CODE("0<>", zero_not_eq): tos = (tos != 0); NEXT;
- CODE("0<", zero_less_than): tos = (tos < 0); NEXT;
- CODE("0>", zero_greater_than): tos = (tos > 0); NEXT;
- CODE("invert", invert): tos = ~tos; NEXT;
- CODE("2/", two_div): tos >>= 1; NEXT;
- CODE("2*", two_times): tos <<= 1; NEXT;
- CODE("lshift", lshift): tos <<= *--sp; NEXT;
- CODE("rshift", rshift): tos >>= *--sp; NEXT;
- CODE("!", store): *((cell*)tos) = *--sp; pop(); NEXT;
- CODE("@", fetch): tos = *((cell*)tos); NEXT;
- CODE("+!", addstore): *((cell*)tos) += *--sp; pop(); NEXT;
- CODE("c!", store_byte): *((char*)tos) = (char)*--sp; pop(); NEXT;
- CODE("c@", fetch_byte): tos = (cell)(*((char*)tos)); NEXT;
+ CODE("1+", one_plus):
+  tos += 1;
+  NEXT;
+ CODE("1-", one_minus):
+  tos -= 1;
+  NEXT;
+ CODE("0=", zero_eq):
+  tos = (tos == 0);
+  NEXT;
+ CODE("0<>", zero_not_eq):
+  tos = (tos != 0);
+  NEXT;
+ CODE("0<", zero_less_than):
+  tos = (tos < 0);
+  NEXT;
+ CODE("0>", zero_greater_than):
+  tos = (tos > 0);
+  NEXT;
+ CODE("invert", invert):
+  tos = ~tos;
+  NEXT;
+ CODE("2/", two_div):
+  tos >>= 1;
+  NEXT;
+ CODE("2*", two_times):
+  tos <<= 1;
+  NEXT;
+ CODE("lshift", lshift):
+  tos <<= *--sp;
+  NEXT;
+ CODE("rshift", rshift):
+  tos >>= *--sp;
+  NEXT;
+ CODE("!", store):
+  *((cell*)tos) = *--sp;
+  pop();
+  NEXT;
+ CODE("@", fetch):
+  tos = *((cell*)tos);
+  NEXT;
+ CODE("+!", addstore):
+  *((cell*)tos) += *--sp;
+  pop();
+  NEXT;
+ CODE("c!", store_byte):
+  *((char*)tos) = (char)*--sp;
+  pop();
+  NEXT;
+ CODE("c@", fetch_byte):
+  tos = (cell)(*((char*)tos));
+  NEXT;
  CODE("cmove", cmove):
   str = (char*)(*--sp);
   cmove((char*)(*--sp), str, (u_cell)tos);
   pop(); NEXT;
- CODE(">r", to_r): rpush(tos); pop(); NEXT;
- CODE("r>", from_r): push(rpop()); NEXT;
- CODE("r@", read_r): push(*(rp - 1)); NEXT;
- CODE("rdrop", r_drop): rp--; NEXT;
- CODE("sp@", sp_fetch): push(sp); NEXT;
- CODE("emit", emit): x = pop(); printf("%c", (char)x); NEXT;
- CODE("create", _create):  parse_name(); create(word_a, word_c); NEXT;
- CODE("allot", allot): dp += pop(); NEXT;
- iCODE("[", lbrack): state = INTERPRET; NEXT;
- CODE("]", rbrack): state = COMPILE; NEXT;
+ CODE(">r", to_r):
+  rpush(tos);
+  pop();
+  NEXT;
+ CODE("r>", from_r):
+  push(rpop());
+  NEXT;
+ CODE("r@", read_r):
+  push(*(rp - 1));
+  NEXT;
+ CODE("rdrop", r_drop):
+  rp--;
+  NEXT;
+ CODE("sp@", sp_fetch):
+  push(sp);
+  NEXT;
+ CODE("emit", emit):
+  x = pop();
+  printf("%c", (char)x);
+  NEXT;
+ CODE("create", _create):
+  parse_name();
+  create(word_a, word_c);
+  NEXT;
+ CODE("allot", allot):
+  dp += pop();
+  NEXT;
+ iCODE("[", lbrack):
+  state = INTERPRET;
+  NEXT;
+ CODE("]", rbrack):
+  state = COMPILE;
+  NEXT;
  CODE("parse-name", _parse_name):
-  parse_name(); push(word_a); push(word_c); NEXT;
+  parse_name();
+  push(word_a);
+  push(word_c);
+  NEXT;
  CODE("find-name", find_name):
   x = pop();
   str = (char*)pop();
   push(find_word(str, x, &imm));
   NEXT;
- CODE("name>int", name_to_int): tos = (cell)cfa((cell*)tos); NEXT;
+ CODE("name>int", name_to_int):
+  tos = (cell)cfa((cell*)tos);
+  NEXT;
  CODE("'", tick):
   parse_name();
   xt = find_word(word_a, word_c, &imm);
   push(cfa(xt));
   NEXT;
- CODE(",", comma): x = pop(); dict_append(x); NEXT;
- CODE("[']", btickb): push(*ip++); NEXT;
+ CODE(",", comma):
+  x = pop();
+  dict_append(x);
+  NEXT;
+ CODE("[']", btickb):
+  push(*ip++);
+  NEXT;
 #define do_branch ip = (cell*)((cell)ip + (cell)(*ip))
- CODE("branch", branch): do_branch; NEXT;
+ CODE("branch", branch):
+  do_branch;
+  NEXT;
  CODE("0branch", zero_branch):
-  x = pop(); if (x==0) do_branch; else ip++; NEXT;
- CODE("litstring", litstring): NEXT; //TODO
- CODE("char", _char): parse_name(); push(*word_a); NEXT;
- CODE(".", dot): xt=(cell*)pop(); printf("%llu\n", (cell)xt); NEXT;
- CODE(".s", print_stack):  print_stack(); NEXT;
- CODE("type", _type):      type((char*)*--sp, tos); tos = *--sp; NEXT;
+  x = pop();
+  if (x==0) {
+    do_branch;
+  }else{
+    ip++;
+  }
+  NEXT;
+ CODE("litstring", litstring):
+  NEXT; //TODO
+ CODE("char", _char):
+  parse_name();
+  push(*word_a);
+  NEXT;
+ CODE(".", dot):
+  xt=(cell*)pop();
+  printf("%llu\n", (cell)xt);
+  NEXT;
+ CODE(".s", print_stack):
+  print_stack();
+  NEXT;
+ CODE("type", _type):
+  type((char*)*--sp, tos);
+  tos = *--sp;
+  NEXT;
  iCODE("execute", execute):
   if (state==COMPILE){
     dict_append(EXEC_XT);
@@ -425,16 +588,37 @@ void forth(){
   xt = (cell*)pop();
   *ip++ = (cell)xt;
   goto **xt;
- CODE("bye", bye): exit(0);
- CODE("literal", _literal): x = pop(); literal(x); NEXT;
- CODE("lit", lit): push(*ip++); NEXT;
- CODE("words", _words): words(); NEXT;
- iCODE("immediate", _immediate): immediate(); NEXT;
- CODE("reveal", reveal): *(cell*)(tos) ^= HIDDEN_BIT; pop(); NEXT;
- CODE("latest", _latest): push(&latest); NEXT;
- CODE("base", _base): push(&base); NEXT;
- CODE("dp", here): push(&dp); NEXT;
- CODE("state", state): push(&state); NEXT;
+ CODE("bye", bye):
+  exit(0);
+ CODE("literal", _literal):
+  x = pop();
+  literal(x);
+  NEXT;
+ CODE("lit", lit):
+  push(*ip++);
+  NEXT;
+ CODE("words", _words):
+  words();
+  NEXT;
+ iCODE("immediate", _immediate):
+  immediate();
+  NEXT;
+ CODE("reveal", reveal):
+  *(cell*)(tos) ^= HIDDEN_BIT;
+  pop();
+  NEXT;
+ CODE("latest", _latest):
+  push(&latest);
+  NEXT;
+ CODE("base", _base):
+  push(&base);
+  NEXT;
+ CODE("dp", here):
+  push(&dp);
+  NEXT;
+ CODE("state", state):
+  push(&state);
+  NEXT;
  CODE(":", colon):
   parse_name();
   create(word_a, word_c);
@@ -442,9 +626,17 @@ void forth(){
   hide();
   state = COMPILE;
   NEXT;
- CODE("see", _see): parse_name(); see(word_a, word_c); NEXT;
- iCODE(";", semi): dict_append(EXIT_XT); state = INTERPRET; NEXT;
- CODE("docol", _docol): push(DOCOL_XT); NEXT;
+ CODE("see", _see):
+  parse_name();
+  see(word_a, word_c);
+  NEXT;
+ iCODE(";", semi):
+  dict_append(EXIT_XT);
+  state = INTERPRET;
+  NEXT;
+ CODE("docol", _docol):
+  push(DOCOL_XT);
+  NEXT;
 }
 
 void init(){
